c37affd1f764a0778a4dfdc6d585cfb6a1dc6a7b
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-08 10:29:06 +0600

    Navigate with Link instead of a

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 const FirstPost = () => {
-	return <h1>First Post</h1>;
+	return (
+		<>
+			<h1>First Post</h1>
+			<Link href="/">
+				<a>Back to home</a>
+			</Link>
+		</>
+	);
 };
 
 export default FirstPost;
