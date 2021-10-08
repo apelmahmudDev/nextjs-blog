d820d1f317cff0b3f69622a80eb19c818c79db27
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-08 13:00:27 +0600

    Utils styles added

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,13 @@
 import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 
 const Layout = ({ children }) => {
-	return <div className={styles.container}>{children}</div>;
+	return (
+		<div className={styles.container}>
+			{children}
+			<h1 className={utilStyles.heading2Xl}> Simple website</h1>
+		</div>
+	);
 };
 
 export default Layout;
