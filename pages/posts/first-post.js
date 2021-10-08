daaf6377a460011c8599c42dbf32d8f33b146335
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-08 11:33:56 +0600

    Now I can set dynamic title using nextjs

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,25 @@
+import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+
 const FirstPost = () => {
 	return (
-		<>
+		<div>
+			<Head>
+				<title>first-post</title>
+			</Head>
+
 			<h1>First Post</h1>
+			<Image
+				src="/assets/images/apel.jpg"
+				alt="apel"
+				height={300}
+				width={300}
+			/>
 			<Link href="/">
 				<a>Back to home</a>
 			</Link>
-		</>
+		</div>
 	);
 };
 
